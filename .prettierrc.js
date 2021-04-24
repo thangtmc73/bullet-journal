ccf39d4cbe2333185bf6b05d521d097e1e529e7a
thangtmc73 <thangtm.dev@gmail.com>
2021-04-24 16:50:05 +0700

    feat(prettier): update config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  'react/jsx-max-props-per-line': [1, { when: 'multiline' }],
+  'react/jsx-first-prop-new-line': 'multiline',
 };
