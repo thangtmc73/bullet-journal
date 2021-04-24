135afc8e91944914d885fa57b5f2fe0fb298bb43
thangtmc73 <thangtm.dev@gmail.com>
2021-04-24 23:37:50 +0700

    feat(color): add more color

--- constants/color.ts
+++ constants/color.ts
@@ -1,3 +1,6 @@
 export const DEFAULT_BACKGROUND = '#f2f2f0';
+export const CONTENT_BACKGROUND = '#ffffff';
 export const BRIGHT_YELLOW = '#ffdb61';
+export const DARK_YELLOW = '#feb83e';
 export const PRIMARY_TEXT = '#000000';
+export const SUB_TEXT = '#a1a1a1';
