0569a6719d3735cce36d60ab73a22e788453cc00
thangtmc73 <thangtm.dev@gmail.com>
2021-04-24 12:35:18 +0700

    feat(app): apply react-navigation with ConventionsPage

---
+++ navigation/MainStack.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import ConventionsPage from '../pages/conventions-page/ConventionsPage';
+import * as ScreenKeys from './screenKeys';
+
+const Stack = createStackNavigator();
+
+function MainStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={ScreenKeys.CONVENTIONS_PAGE}
+        component={ConventionsPage}
+        options={{
+          title: 'Conventions',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default MainStack;
