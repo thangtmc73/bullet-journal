bcae575ef4c19d39f7282fc7acae93d3e9bb9cea
thangtmc73 <thangtm.dev@gmail.com>
2021-04-24 16:55:31 +0700

    feat(conventions-page): show list conventions in slider

--- pages/conventions-page/ConventionsPage.tsx
+++ pages/conventions-page/ConventionsPage.tsx
@@ -1,20 +1,74 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { BulletType } from '../../constants';
+import Section from './section/Section';
+import Slider from 'react-native-app-intro-slider';
 
 function ConventionsPage() {
+  function renderItem({
+    item,
+  }: {
+    item: {
+      title: string;
+      items: Array<number>;
+    };
+  }) {
+    const { title, items } = item;
+    return <Section title={title} items={items} />;
+  }
+
+  function keyExtractor({ title }: { title: string }) {
+    return title;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Conventions</Text>
-    </View>
+    <Slider
+      data={listConventions}
+      keyExtractor={keyExtractor}
+      horizontal={true}
+      renderItem={renderItem}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+const listConventions = [
+  {
+    title: 'Task',
+    items: [
+      BulletType.TASK,
+      BulletType.TASK_COMPLETED,
+      BulletType.TASK_MIGRATED,
+      BulletType.TASK_CANCELLED,
+    ],
   },
-});
+  {
+    title: 'Event',
+    items: [
+      BulletType.EVENT,
+      BulletType.EVENT_COMPLETED,
+      BulletType.EVENT_MIGRATED,
+      BulletType.EVENT_CANCELLED,
+    ],
+  },
+  {
+    title: 'Appointment',
+    items: [
+      BulletType.APPOINTMENT,
+      BulletType.APPOINTMENT_COMPLETED,
+      BulletType.APPOINTMENT_MIGRATED,
+      BulletType.APPOINTMENT_CANCELLED,
+    ],
+  },
+  {
+    title: 'Others',
+    items: [
+      BulletType.OTHER_NOTES,
+      BulletType.OTHER_DUE_DATE,
+      BulletType.OTHER_IMPORTTANT,
+      BulletType.OTHER_REMINDER,
+      BulletType.OTHER_EXPENSES,
+      BulletType.OTHER_QUESTION,
+    ],
+  },
+];
 
 export default ConventionsPage;
