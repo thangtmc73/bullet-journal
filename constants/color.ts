fdcae06a270fce9799f9258e87f4b2ebe83966e0
thangtmc73 <thangtm.dev@gmail.com>
2021-04-25 13:54:26 +0700

    feat(constants): update colors

--- constants/color.ts
+++ constants/color.ts
@@ -1,6 +1,8 @@
 export const DEFAULT_BACKGROUND = '#f2f2f0';
 export const CONTENT_BACKGROUND = '#ffffff';
+export const VERY_LIGHT_YELLOW = '#fff3d4';
 export const BRIGHT_YELLOW = '#ffdb61';
 export const DARK_YELLOW = '#feb83e';
 export const PRIMARY_TEXT = '#000000';
 export const SUB_TEXT = '#a1a1a1';
+export const SEPARATOR = '#8f8f8f';
