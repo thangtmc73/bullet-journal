cdee39732f2b9e1a4c2708d7fac910c7a1b9e31a
thangtmc73 <thangtm.dev@gmail.com>
2021-04-25 09:02:39 +0700

    feat(constants): use new screen key

--- navigation/MainStack.tsx
+++ navigation/MainStack.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ConventionsPage from '../pages/conventions-page/ConventionsPage';
 import DayDetailPage from '../pages/day-detail-page/DayDetailPage';
-import * as ScreenKeys from './screenKeys';
+import { ScreenKey } from '../constants';
 
 const Stack = createStackNavigator();
 
 function MainStack() {
   return (
-    <Stack.Navigator initialRouteName={ScreenKeys.DAY_DETAIL_PAGE}>
+    <Stack.Navigator initialRouteName={ScreenKey.CONVENTIONS_PAGE}>
       <Stack.Screen
-        name={ScreenKeys.CONVENTIONS_PAGE}
+        name={ScreenKey.CONVENTIONS_PAGE}
         component={ConventionsPage}
         options={{
           title: 'Conventions',
         }}
       />
       <Stack.Screen
-        name={ScreenKeys.DAY_DETAIL_PAGE}
+        name={ScreenKey.DAY_DETAIL_PAGE}
         component={DayDetailPage}
         options={{
           title: 'Today',
